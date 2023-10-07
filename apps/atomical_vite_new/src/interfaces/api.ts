6eb00e309f2560fe8dd08b3021d84fafaa9fbf39
neeboo <ghostcorn@gmail.com>
2023-10-08 01:11:57 +0800

    :sparkles: added new

--- apps/atomical_vite_new/src/interfaces/api.ts
+++ apps/atomical_vite_new/src/interfaces/api.ts
@@ -1,4 +1,4 @@
-import { UTXO } from "./utxo";
+import { UTXO } from './utxo';
 
 export interface IUnspentResponse {
   confirmed: number;
@@ -10,17 +10,12 @@ export interface IUnspentResponse {
 export interface ElectrumApiInterface {
   // close: () => Promise<void>;
   // open: () => Promise<boolean>;
-  resetConnection: () => Promise<void>;
+  resetConnection: () => Promise<boolean | void>;
   // isOpen: () => boolean;
   sendTransaction: (rawtx: string) => Promise<string>;
   getUnspentAddress: (address: string) => Promise<IUnspentResponse>;
   getUnspentScripthash: (address: string) => Promise<IUnspentResponse>;
-  waitUntilUTXO: (
-    address: string,
-    satoshis: number,
-    sleepTimeSec: number,
-    exactSatoshiAmount?: boolean
-  ) => Promise<any>;
+  waitUntilUTXO: (address: string, satoshis: number, sleepTimeSec: number, exactSatoshiAmount?: boolean) => Promise<any>;
   getTx: (txid: string, verbose?: boolean) => Promise<any>;
   serverVersion: () => Promise<any>;
   broadcast: (rawtx: string) => Promise<any>;
@@ -29,54 +24,27 @@ export interface ElectrumApiInterface {
   atomicalsGetGlobal: () => Promise<any>;
   atomicalsGet: (atomicalAliasOrId: string | number) => Promise<any>;
   atomicalsGetLocation: (atomicalAliasOrId: string | number) => Promise<any>;
-  atomicalsGetState: (
-    atomicalAliasOrId: string | number,
-    path: string,
-    verbose: boolean
-  ) => Promise<any>;
-  atomicalsGetStateHistory: (
-    atomicalAliasOrId: string | number
-  ) => Promise<any>;
-  atomicalsGetEventHistory: (
-    atomicalAliasOrId: string | number
-  ) => Promise<any>;
+  atomicalsGetState: (atomicalAliasOrId: string | number, path: string, verbose: boolean) => Promise<any>;
+  atomicalsGetStateHistory: (atomicalAliasOrId: string | number) => Promise<any>;
+  atomicalsGetEventHistory: (atomicalAliasOrId: string | number) => Promise<any>;
   atomicalsGetTxHistory: (atomicalAliasOrId: string | number) => Promise<any>;
   atomicalsList: (limit: number, offset: number, asc: boolean) => Promise<any>;
-  atomicalsByScripthash: (
-    scripthash: string,
-    verbose?: boolean
-  ) => Promise<any>;
+  atomicalsByScripthash: (scripthash: string, verbose?: boolean) => Promise<any>;
   atomicalsByAddress: (address: string) => Promise<any>;
   atomicalsAtLocation: (location: string) => Promise<any>;
   atomicalsGetByRealm: (realm: string) => Promise<any>;
-  atomicalsGetRealmInfo: (
-    realmOrSubRealm: string,
-    verbose?: boolean
-  ) => Promise<any>;
+  atomicalsGetRealmInfo: (realmOrSubRealm: string, verbose?: boolean) => Promise<any>;
   atomicalsGetByTicker: (ticker: string) => Promise<any>;
   atomicalsGetByContainer: (container: string) => Promise<any>;
-  atomicalsFindTickers: (
-    tickerPrefix: string | null,
-    asc?: boolean
-  ) => Promise<any>;
-  atomicalsFindContainers: (
-    containerPrefix: string | null,
-    asc?: boolean
-  ) => Promise<any>;
-  atomicalsFindRealms: (
-    realmPrefix: string | null,
-    asc?: boolean
-  ) => Promise<any>;
-  atomicalsFindSubRealms: (
-    parentRealmId: string,
-    subrealmPrefix: string | null,
-    mostRecentFirst?: boolean
-  ) => Promise<any>;
+  atomicalsFindTickers: (tickerPrefix: string | null, asc?: boolean) => Promise<any>;
+  atomicalsFindContainers: (containerPrefix: string | null, asc?: boolean) => Promise<any>;
+  atomicalsFindRealms: (realmPrefix: string | null, asc?: boolean) => Promise<any>;
+  atomicalsFindSubRealms: (parentRealmId: string, subrealmPrefix: string | null, mostRecentFirst?: boolean) => Promise<any>;
 }
 
 export interface IAtomicalBalanceSummary {
   confirmed: number;
-  type: "FT" | "NFT";
+  type: 'FT' | 'NFT';
   atomical_number?: number;
   atomical_id?: number;
   $ticker?: string;
@@ -101,7 +69,7 @@ export interface AmountToSend {
 
 export interface IAtomicalsInfo {
   confirmed: number;
-  type: "FT" | "NFT";
+  type: 'FT' | 'NFT';
   utxos: ISelectedUtxo[];
 }
 
@@ -110,7 +78,7 @@ export interface IAtomicalBalances {
 }
 
 enum TickerStatus {
-  "verified" = "verified",
+  'verified' = 'verified',
 }
 
 export interface TickerCandidate {
@@ -131,10 +99,10 @@ export interface IAtomicalBalanceItem {
     note: string;
     verified_atomical_id: string;
   };
-  subtype: "decentralized";
+  subtype: 'decentralized';
   ticker: string;
   ticker_candidate: TickerCandidate[];
-  type: "FT" | "NFT";
+  type: 'FT' | 'NFT';
 }
 
 export interface IAtomicalBalanceItemData {
@@ -160,8 +128,8 @@ export interface IAtomicalBalanceItemData {
     confirmed: boolean;
     mint_data?: any; // todo
     mint_info?: any; // dodo
-    subtype: "decentralized";
-    type: "FT" | "NFT";
+    subtype: 'decentralized';
+    type: 'FT' | 'NFT';
     // todo
   };
   $container?: string;
--- apps/atomical_vite_new/src/services/hooks/index.ts
+++ apps/atomical_vite_new/src/services/hooks/index.ts
@@ -1,16 +1,15 @@
-import { ElectrumApi } from "@/clients/eletrum";
-import { AtomicalService } from "../atomical";
-import { useEffect, useRef, useState } from "react";
-import { IAtomicalBalances, ISelectedUtxo } from "@/interfaces/api";
-import { UTXO } from "@/interfaces/utxo";
-import { AstroXWizzInhouseProvider } from "webf_wizz_inhouse";
-import { fromPubToP2tr, toXOnly } from "@/clients/utils";
-import { MempoolUtxo, mempoolService } from "@/clients/mempool";
+import { ElectrumApi } from '@/clients/eletrum';
+import { AtomicalService } from '../atomical';
+import { useEffect, useRef, useState } from 'react';
+import { IAtomicalBalances, ISelectedUtxo } from '@/interfaces/api';
+import { UTXO } from '@/interfaces/utxo';
+import { AstroXWizzInhouseProvider } from 'webf_wizz_inhouse';
+import { fromPubToP2tr, toXOnly } from '@/clients/utils';
+import { MempoolUtxo, mempoolService } from '@/clients/mempool';
 const provider = new AstroXWizzInhouseProvider();
 
-const ELECTRUMX_HTTP_PROXY = "https://ep.atomicals.xyz/proxy";
-const api = ElectrumApi.createClient(ELECTRUMX_HTTP_PROXY);
-//@ts-ignore
+const ELECTRUMX_WSS = 'ws://18.139.208.6:50001';
+const api = ElectrumApi.createClient(ELECTRUMX_WSS);
 const atomicalService = new AtomicalService(api);
 
 interface UseAtomicalService {
@@ -41,13 +40,9 @@ export function useAtomicalService(): UseAtomicalService {
 export function useAtomicalWalletInfo(address: string) {
   const { service } = useAtomicalService();
   const [balance, setBalance] = useState<number | undefined>(undefined);
-  const [fundingBalance, setFundingBalance] = useState<number | undefined>(
-    undefined
-  );
+  const [fundingBalance, setFundingBalance] = useState<number | undefined>(undefined);
   const [nonAtomUtxos, setNonAtomUtxos] = useState<UTXO[]>([]);
-  const [balanceMap, setBalanceMap] = useState<IAtomicalBalances | undefined>(
-    undefined
-  );
+  const [balanceMap, setBalanceMap] = useState<IAtomicalBalances | undefined>(undefined);
   const [allUtxos, setAllUxtos] = useState<UTXO[]>([]);
   const [atomUtxos, setAtomUtxos] = useState<ISelectedUtxo[]>([]);
   const [error, setError] = useState<string | undefined>(undefined);
@@ -69,11 +64,7 @@ export function useAtomicalWalletInfo(address: string) {
       const mempoolUtxos: MempoolUtxo[] = await mempoolService.getUtxo(address);
       const confirmedUtxos: UTXO[] = [];
       for (let i = 0; i < _allUtxos.utxos.length; i++) {
-        const found = mempoolUtxos.findIndex(
-          (item) =>
-            item.txid === _allUtxos.utxos[i].txid &&
-            item.status.confirmed === true
-        );
+        const found = mempoolUtxos.findIndex(item => item.txid === _allUtxos.utxos[i].txid && item.status.confirmed === true);
         if (found > -1) {
           confirmedUtxos.push(_allUtxos.utxos[i]);
         }
@@ -102,9 +93,7 @@ export function useAtomicalWalletInfo(address: string) {
       if (total === 0 || total === undefined) {
         for (let i = 0; i < confirmedUtxos.length; i++) {
           const utxo = confirmedUtxos[i];
-          if (
-            atomicals_utxos.findIndex((item) => item.txid === utxo.txid) < 0
-          ) {
+          if (atomicals_utxos.findIndex(item => item.txid === utxo.txid) < 0) {
             nonAtomUtxos.push(utxo);
             nonAtomUtxosValue += utxo.value;
           }
@@ -112,20 +101,14 @@ export function useAtomicalWalletInfo(address: string) {
       } else {
         for (let i = 0; i < confirmedUtxos.length; i++) {
           const utxo = confirmedUtxos[i];
-          if (
-            atomicals_utxos.findIndex((item) => item.txid === utxo.txid) < 0
-          ) {
+          if (atomicals_utxos.findIndex(item => item.txid === utxo.txid) < 0) {
             _nonAtomUtxos.push(utxo);
           }
         }
 
         for (let j = 0; j < _nonAtomUtxos.length; j++) {
           const utxo = _nonAtomUtxos[j];
-          if (
-            ordList.findIndex(
-              (item) => item.output.split(":")[0] === utxo.txId
-            ) < 0
-          ) {
+          if (ordList.findIndex(item => item.output.split(':')[0] === utxo.txId) < 0) {
             nonAtomUtxos.push(utxo);
             nonAtomUtxosValue += utxo.value;
           }
@@ -152,15 +135,11 @@ export function useAtomicalWalletInfo(address: string) {
 
 export function useAddress() {
   const [address, setAddress] = useState<string | undefined>(undefined);
-  const [originAddress, setOriginAddress] = useState<string | undefined>(
-    undefined
-  );
+  const [originAddress, setOriginAddress] = useState<string | undefined>(undefined);
   const [addressType, setAddressType] = useState<string | undefined>(); // ['p2pkh', 'p2tr', 'p2wpkh']
   const [accs, setAccs] = useState<string[]>([]);
   const [xonlyPubHex, setXonlyPubHex] = useState<string | undefined>(undefined);
-  const [isAllowedAddressType, setIsAllowedAddressType] = useState<
-    boolean | undefined
-  >(undefined);
+  const [isAllowedAddressType, setIsAllowedAddressType] = useState<boolean | undefined>(undefined);
   useEffect(() => {
     (async () => {
       try {
@@ -170,15 +149,13 @@ export function useAddress() {
         setOriginAddress(accs[0]);
         const currentAddressType = await provider.getAddressType(accs[0]);
         setAddressType(currentAddressType);
-        const xpub = (toXOnly(Buffer.from(p2trPub, "hex")) as Buffer).toString(
-          "hex"
-        );
+        const xpub = (toXOnly(Buffer.from(p2trPub, 'hex')) as Buffer).toString('hex');
         setXonlyPubHex(xpub);
         // setXonlyPubHex('133c85d348d6c0796382966380719397453592e706cd3329119a2d2cb8d2ff7b');
         const p2trAddress = fromPubToP2tr(p2trPub);
         // const p2trAddress = 'bc1pgvdp7lf89d62zadds5jvyjntxmr7v70yv33g7vqaeu2p0cuexveq9hcwdv'; //fromPubToP2tr(p2trPub);
         setAddress(p2trAddress);
-        if ((await provider.getAddressType(accs[0])) === "p2pkh") {
+        if ((await provider.getAddressType(accs[0])) === 'p2pkh') {
           setIsAllowedAddressType(true);
         } else {
           setIsAllowedAddressType(false);
@@ -186,11 +163,8 @@ export function useAddress() {
         setAddress(p2trAddress);
         setIsAllowedAddressType(true);
       } catch (err) {
-        setXonlyPubHex(
-          "133c85d348d6c0796382966380719397453592e706cd3329119a2d2cb8d2ff7b"
-        );
-        const p2trAddress =
-          "bc1pgvdp7lf89d62zadds5jvyjntxmr7v70yv33g7vqaeu2p0cuexveq9hcwdv"; //fromPubToP2tr(p2trPub);
+        setXonlyPubHex('133c85d348d6c0796382966380719397453592e706cd3329119a2d2cb8d2ff7b');
+        const p2trAddress = 'bc1pgvdp7lf89d62zadds5jvyjntxmr7v70yv33g7vqaeu2p0cuexveq9hcwdv'; //fromPubToP2tr(p2trPub);
         setAddress(p2trAddress);
         setIsAllowedAddressType(true);
       }
